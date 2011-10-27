186d486644a0082013f7d1162b161bb97401c1f0
Brian (Pololu) <dev-brian@pololu.com>
2011-10-26 17:40:05 -0700

    Release candidate.

--- DualVNH5019MotorShield/DualVNH5019MotorShield.cpp
+++ DualVNH5019MotorShield/DualVNH5019MotorShield.cpp
@@ -1,173 +1,183 @@
 #include "DualVNH5019MotorShield.h"
 #include "pins_arduino.h"
 
+// Constructors ////////////////////////////////////////////////////////////////
+
 DualVNH5019MotorShield::DualVNH5019MotorShield()
 {
-	//Pin map
-	INB1 = 4;
-	INA1 = 2;
-	INA2 = 7;
-	INB2 = 8;
-	EN1DIAG1 = 6;
-	EN2DIAG2 = 12;
-	PWM1 = 9;
-	PWM2 = 10;
-	CS1 = 0;
-	CS2 = 1;
+  //Pin map
+  _INA1 = 2;
+  _INB1 = 4;
+  _PWM1 = 9;
+  _EN1DIAG1 = 6;
+  _CS1 = A0; 
+  _INA2 = 7;
+  _INB2 = 8;
+  _PWM2 = 10;
+  _EN2DIAG2 = 12;
+  _CS2 = A1;
 }
 
-DualVNH5019MotorShield::DualVNH5019MotorShield(int _INB1, int _INA1, int _INA2, int _INB2, int _EN1, int _EN2)
+DualVNH5019MotorShield::DualVNH5019MotorShield(unsigned char INA1, unsigned char INB1, unsigned char EN1DIAG1, unsigned char CS1, 
+                                           unsigned char INA2, unsigned char INB2, unsigned char EN2DIAG2, unsigned char CS2)
 {
-	//Pin map
-	INB1 = _INB1;
-	INA1 = _INA1;
-	INA2 = _INA2;
-	INB2 = _INB2;
-	EN1DIAG1 = _EN1;
-	EN2DIAG2 = _EN2;
-	//PWM1 and PWM2 cannot be remapped because the library assumes PWM is on timer1
-	PWM1 = 9;
-	PWM2 = 10;
+  //Pin map
+  //PWM1 and PWM2 cannot be remapped because the library assumes PWM is on timer1
+  _INA1 = INA1;
+  _INB1 = INB1;
+  _PWM1 = 9;
+  _EN1DIAG1 = EN1DIAG1;
+  _CS1 = CS1;
+  _INA2 = INA2;
+  _INB2 = INB2;
+  _PWM2 = 10;
+  _EN2DIAG2 = EN2DIAG2;
+  _CS2 = CS2;
 }
 
+// Public Methods //////////////////////////////////////////////////////////////
+
+// Define pinMode for the pins and set the frequency for timer1.
 void DualVNH5019MotorShield::init()
 {
-	pinMode(INB1,OUTPUT);
-	pinMode(INA1,OUTPUT);
-	pinMode(INA2,OUTPUT);
-	pinMode(INB2,OUTPUT);
-	pinMode(PWM1,OUTPUT);
-	pinMode(PWM2,OUTPUT);
-	pinMode(EN1DIAG1,INPUT);
-	pinMode(EN2DIAG2,INPUT);
-	pinMode(CS1,INPUT);
-	pinMode(CS2,INPUT);
-	//PWM
-	TCCR1A = 0b10100000;// clkI/O /1 prescaler
-	TCCR1B = 0b00010001;
-	ICR1 = 400; // 400 gives 20kHz
+  pinMode(_INA1,OUTPUT);
+  pinMode(_INB1,OUTPUT);
+  pinMode(_PWM1,OUTPUT);
+  pinMode(_EN1DIAG1,INPUT);
+  pinMode(_CS1,INPUT);
+  pinMode(_INA2,OUTPUT);
+  pinMode(_INB2,OUTPUT);
+  pinMode(_PWM2,OUTPUT);
+  pinMode(_EN2DIAG2,INPUT);
+  pinMode(_CS2,INPUT);
+  
+  // Timer 1 configuration
+  // prescaler: clockI/O / 1
+  // outputs enabled
+  // phase-correct PWM
+  // top of 400
+  //
+  // PWM frequency calculation
+  // 16MHz / 1 (prescaler) / 2 (phase-correct) / 400 (top) = 20kHz
+  TCCR1A = 0b10100000;
+  TCCR1B = 0b00010001;
+  ICR1 = 400;
 }
 
+// Set speed for motor 1, speed is a number betwenn 0 and 400
 void DualVNH5019MotorShield::setM1Speed(int speed)
 {
-	unsigned char reverse = 0;
-	
-	if (speed < 0)
-	{
-		speed = -speed;	// make speed a positive quantity
-		reverse = 1;	// preserve the direction
-	}
-	if (speed > 400)	// Max PWM dutycycle
-		speed = 400;
-	OCR1A = speed;
-	if (speed == 0)
-	{
-		setM1Brake(0);
-	}
-	else
-	{
-		if (reverse)
-		{
-			digitalWrite(INA1,LOW);
-			digitalWrite(INB1,HIGH);
-		}
-		else
-		{
-			digitalWrite(INA1,HIGH);
-			digitalWrite(INB1,LOW);
-		}
-	}
+  unsigned char reverse = 0;
+  
+  if (speed < 0)
+  {
+    speed = -speed;  // Make speed a positive quantity
+    reverse = 1;  // Preserve the direction
+  }
+  if (speed > 400)  // Max PWM dutycycle
+    speed = 400;
+  OCR1A = speed;
+  if (reverse)
+  {
+    digitalWrite(_INA1,LOW);
+    digitalWrite(_INB1,HIGH);
+  }
+  else
+  {
+    digitalWrite(_INA1,HIGH);
+    digitalWrite(_INB1,LOW);
+  }
 }
 
+// Set speed for motor 2, speed is a number betwenn 0 and 400
 void DualVNH5019MotorShield::setM2Speed(int speed)
 {
-	unsigned char reverse = 0;
-	
-	if (speed < 0)
-	{
-		speed = -speed;	// make speed a positive quantity
-		reverse = 1;	// preserve the direction
-	}
-	if (speed > 400)	// Max 
-		speed = 400;
-	OCR1B = speed;
-	if (speed == 0)
-	{
-		setM2Brake(0);
-	}
-	else
-	{
-		if (reverse)
-		{
-			digitalWrite(INA2,LOW);
-			digitalWrite(INB2,HIGH);
-		}
-		else
-		{
-			digitalWrite(INA2,HIGH);
-			digitalWrite(INB2,LOW);
-		}
-	}
+  unsigned char reverse = 0;
+  
+  if (speed < 0)
+  {
+    speed = -speed;  // make speed a positive quantity
+    reverse = 1;  // preserve the direction
+  }
+  if (speed > 400)  // Max 
+    speed = 400;
+  OCR1B = speed;
+  if (reverse)
+  {
+    digitalWrite(_INA2,LOW);
+    digitalWrite(_INB2,HIGH);
+  }
+  else
+  {
+    digitalWrite(_INA2,HIGH);
+    digitalWrite(_INB2,LOW);
+  }
 }
 
+// Set speed for motor 1 and 2
 void DualVNH5019MotorShield::setSpeeds(int m1Speed, int m2Speed)
 {
-	setM1Speed(m1Speed);
-	setM2Speed(m2Speed);
+  setM1Speed(m1Speed);
+  setM2Speed(m2Speed);
 }
 
-// coastDutyCycle is a number between 0 and 400
-void DualVNH5019MotorShield::setM1Brake(int coastDutyCycle)
+// Brake motor 1, brake is a number between 0 and 400
+void DualVNH5019MotorShield::setM1Brake(int brake)
 {
-	// normalize coastDutyCycle
-	if (coastDutyCycle < 0)
-	{
-		coastDutyCycle = -coastDutyCycle;
-	}
-	if (coastDutyCycle > 400)	// Max dutyCycle
-		coastDutyCycle = 400;
-	digitalWrite(INA1, LOW);
-	digitalWrite(INB1, LOW);
-	OCR1A = coastDutyCycle;
+  // normalize brake
+  if (brake < 0)
+  {
+    brake = -brake;
+  }
+  if (brake > 400)  // Max brake
+    brake = 400;
+  digitalWrite(_INA1, LOW);
+  digitalWrite(_INB1, LOW);
+  OCR1A = brake;
 }
 
-// coastDutyCycle is a number between 0 and 400
-void DualVNH5019MotorShield::setM2Brake(int coastDutyCycle)
+// Brake motor 2, brake is a number between 0 and 400
+void DualVNH5019MotorShield::setM2Brake(int brake)
 {
-	// normalize coastDutyCycle
-	if (coastDutyCycle < 0)
-	{
-		coastDutyCycle = -coastDutyCycle;
-	}
-	if (coastDutyCycle > 400)	// Max dutyCycle
-		coastDutyCycle = 400;
-	digitalWrite(INA2, LOW);
-	digitalWrite(INB2, LOW);
-	OCR1B = coastDutyCycle;
+  // normalize brake
+  if (brake < 0)
+  {
+    brake = -brake;
+  }
+  if (brake > 400)  // Max brake
+    brake = 400;
+  digitalWrite(_INA2, LOW);
+  digitalWrite(_INB2, LOW);
+  OCR1B = brake;
 }
 
-void DualVNH5019MotorShield::brake(int coastDutyCycle)
+// Brake motor 1 and 2, brake is a number between 0 and 400
+void DualVNH5019MotorShield::setBrakes(int m1Brake, int m2Brake)
 {
-	setM1Brake(coastDutyCycle);
-	setM2Brake(coastDutyCycle);
+  setM1Brake(m1Brake);
+  setM2Brake(m2Brake);
 }
 
-int DualVNH5019MotorShield::getM1CurrentMilliamps()
+// Return motor 1 current value in mA.
+unsigned int DualVNH5019MotorShield::getM1CurrentMilliamps()
 {
-	return analogRead(CS1) * 23;
+  return analogRead(_CS1) * 23;
 }
 
-int DualVNH5019MotorShield::getM2CurrentMilliamps()
+// Return motor 2 current value in mA.
+unsigned int DualVNH5019MotorShield::getM2CurrentMilliamps()
 {
-	return analogRead(CS2) * 23;
+  return analogRead(_CS2) * 23;
 }
 
-bool DualVNH5019MotorShield::getM1Error()
+// Return error status for motor 1 
+unsigned char DualVNH5019MotorShield::getM1Fault()
 {
-	return digitalRead(EN1DIAG1);
+  return !digitalRead(_EN1DIAG1);
 }
 
-bool DualVNH5019MotorShield::getM2Error()
+// Return error status for motor 2 
+unsigned char DualVNH5019MotorShield::getM2Fault()
 {
-	return digitalRead(EN2DIAG2);
+  return !digitalRead(_EN2DIAG2);
 }
--- DualVNH5019MotorShield/DualVNH5019MotorShield.h
+++ DualVNH5019MotorShield/DualVNH5019MotorShield.h
@@ -1,44 +1,41 @@
 #ifndef DualVNH5019MotorShield_h
 #define DualVNH5019MotorShield_h
-#include <WProgram.h>
-#include "pins_arduino.h"
+
+#include <WProgram.h> // For byte data type
+#include "pins_arduino.h" // Pin defination function for Arduino.
 
 class DualVNH5019MotorShield
 {
-	public:
-		//CONSTRUCTORS
-		DualVNH5019MotorShield(); //default
-		DualVNH5019MotorShield(int INB1, int INA1, int INA2, int INB2, int EN1, int EN2); //user define pins
-		
-		void init();
-		
-		// sets the motor speed.  The sign of 'speed' determines the direction
-		// and the magnitude determines the speed.  limits: -400 <= speed <= 400
-		// |speed| = 400 produces the maximum speed while speed = 0 is full brake.
-		void setM1Speed(int speed);
-		void setM2Speed(int speed);
-		void setSpeeds(int m1Speed, int m2Speed);
-		void setM1Brake(int coastDutyCycle);
-		void setM2Brake(int coastDutyCycle);
-		void brake(int coastDutyCycle);// choose between coasting mode and brake low.
-		int getM1CurrentMilliamps();
-		int getM2CurrentMilliamps();
-		bool getM1Error();
-		bool getM2Error();
-		
-	private:
-		int INB1;
-		int INA1;
-		int PWM1;
-		int PWM2;
-		int INA2;
-		int INB2;
-		int EN1DIAG1;
-		int EN2DIAG2;
-		int CS1;
-		int CS2;
-		
-		
+  public:
+    // CONSTRUCTORS
+    DualVNH5019MotorShield(); // Default pin selection.
+    DualVNH5019MotorShield(unsigned char INA1, unsigned char INB1, unsigned char EN1DIAG1, unsigned char CS1, 
+                          unsigned char INA2, unsigned char INB2, unsigned char EN2DIAG2, unsigned char CS2); // User-defined pin selection. 
+    
+    // PUBLIC METHODS
+    void init(); // Initialize TIMER 1, set the PWM to 20kHZ. 
+    void setM1Speed(int speed); // Set speed for M1.
+    void setM2Speed(int speed); // Set speed for M2.
+    void setSpeeds(int m1Speed, int m2Speed); // Set speed for both M1 and M2.
+    void setM1Brake(int brake); // Brake M1. 
+    void setM2Brake(int brake); // Brake M2.
+    void setBrakes(int m1Brake, int m2Brake); // Brake both M1 and M2.
+    unsigned int getM1CurrentMilliamps(); // Get current reading for M1. 
+    unsigned int getM2CurrentMilliamps(); // Get current reading for M2.
+    unsigned char getM1Fault(); // Get fault reading from M1.
+    unsigned char getM2Fault(); // Get fault reading from M2.
+    
+  private:
+    unsigned char _INA1;
+    unsigned char _INB1;
+    unsigned char _PWM1;
+    unsigned char _EN1DIAG1;
+    unsigned char _CS1;
+    unsigned char _INA2;
+    unsigned char _INB2;
+    unsigned char _PWM2;
+    unsigned char _EN2DIAG2;
+    unsigned char _CS2;  
 };
 
 #endif
