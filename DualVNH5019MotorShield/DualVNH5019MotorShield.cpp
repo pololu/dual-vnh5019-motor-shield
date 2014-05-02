8bb6a9dbfc6df49453b9f402618708d5a3d90a19
Sam Miller <sm11963@gmail.com>
2014-05-01 23:49:22 -0400

    Added selectable PWM pins
    You can now select PWM pins and the library will check if timers can be
    used with those pins. If so, the 16bit timer will be used, otherwise
    analogWrite will be used.
    
    Added support of Arduino Mega2560.

--- DualVNH5019MotorShield/DualVNH5019MotorShield.cpp
+++ DualVNH5019MotorShield/DualVNH5019MotorShield.cpp
@@ -9,14 +9,19 @@ DualVNH5019MotorShield::DualVNH5019MotorShield()
   _INB1 = 4;
   _EN1DIAG1 = 6;
   _CS1 = A0; 
+  _PWM1 = 9;
   _INA2 = 7;
   _INB2 = 8;
   _EN2DIAG2 = 12;
   _CS2 = A1;
+  _PWM2 = 10;
 }
 
-DualVNH5019MotorShield::DualVNH5019MotorShield(unsigned char INA1, unsigned char INB1, unsigned char EN1DIAG1, unsigned char CS1, 
-                                               unsigned char INA2, unsigned char INB2, unsigned char EN2DIAG2, unsigned char CS2)
+DualVNH5019MotorShield::DualVNH5019MotorShield(unsigned char INA1, unsigned char INB1, 
+											   unsigned char EN1DIAG1, unsigned char CS1, 
+                                               unsigned char INA2, unsigned char PWM1,
+											   unsigned char INB2, unsigned char EN2DIAG2, 
+											   unsigned char CS2, unsigned char PWM2)
 {
   //Pin map
   //PWM1 and PWM2 cannot be remapped because the library assumes PWM is on timer1
@@ -24,10 +29,12 @@ DualVNH5019MotorShield::DualVNH5019MotorShield(unsigned char INA1, unsigned char
   _INB1 = INB1;
   _EN1DIAG1 = EN1DIAG1;
   _CS1 = CS1;
+  _PWM1 = PWM1;
   _INA2 = INA2;
   _INB2 = INB2;
   _EN2DIAG2 = EN2DIAG2;
   _CS2 = CS2;
+  _PWM2 = PWM2;
 }
 
 // Public Methods //////////////////////////////////////////////////////////////
@@ -45,6 +52,11 @@ void DualVNH5019MotorShield::init()
   pinMode(_PWM2,OUTPUT);
   pinMode(_EN2DIAG2,INPUT);
   pinMode(_CS2,INPUT);
+  
+  // initialize the register pointers to null. If not set we will use analogWrite
+  _PWM1_REG = NULL; 
+  _PWM2_REG = NULL;
+  
   #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
   // Timer 1 configuration
   // prescaler: clockI/O / 1
@@ -54,9 +66,88 @@ void DualVNH5019MotorShield::init()
   //
   // PWM frequency calculation
   // 16MHz / 1 (prescaler) / 2 (phase-correct) / 400 (top) = 20kHz
-  TCCR1A = 0b10100000;
-  TCCR1B = 0b00010001;
-  ICR1 = 400;
+  if(_PWM1 == 9 || _PWM1 == 10 || _PWM2 == 9 || _PWM2 == 10)
+  {
+    if(_PWM1 == 9)       { TCCR1A |= COM1A1; _PWM1_REG = &OCR1A; }
+	else if(_PWM1 == 10) { TCCR1A |= COM1B1; _PWM1_REG = &OCR1B; }
+	
+	if(_PWM2 == 9)       { TCCR1A |= COM1A1; _PWM2_REG = &OCR1A; }
+	else if(_PWM2 == 10) { TCCR1A |= COM1B1; _PWM2_REG = &OCR1B; }
+  
+	TCCR1A = COM1A1 + COM1B1;
+	TCCR1B = WGM13 + CS10;
+	ICR1 = 400;
+  }
+  /* ------------------------------- FOR THE MEGA ------------------------- */
+  #elif defined(__AVR_ATmega2560__) 
+  if(_PWM1 == 11 || _PWM1 == 12 || _PWM1 == 13 || 
+     _PWM2 == 11 || _PWM2 == 12 || _PWM2 == 13)
+  {
+    //timer1
+	TCCR1A = 0;
+	if(_PWM1 == 11)      { TCCR1A |= COM1A1; _PWM1_REG = &OCR1A; }
+	else if(_PWM1 == 12) { TCCR1A |= COM1B1; _PWM1_REG = &OCR1B; }
+	else if(_PWM1 == 13) { TCCR1A |= COM1C1; _PWM1_REG = &OCR1C; }
+	
+	if(_PWM2 == 11)      { TCCR1A |= COM1A1; _PWM2_REG = &OCR1A; }
+	else if(_PWM2 == 12) { TCCR1A |= COM1B1; _PWM2_REG = &OCR1B; }
+	else if(_PWM2 == 13) { TCCR1A |= COM1C1; _PWM2_REG = &OCR1C; }
+	
+	//TCCR1A = COM1A1 + COM1B1 + COM1C1;
+    TCCR1B = WGM13 + CS10;
+    ICR1 = 400;
+  }
+  else if(_PWM1 == 2 || _PWM1 == 3 || _PWM1 == 5 || 
+          _PWM2 == 2 || _PWM2 == 3 || _PWM2 == 5)
+  {
+    //timer3
+	TCCR3A = 0;
+	if(_PWM1 == 5)      { TCCR3A |= COM3A1; _PWM1_REG = &OCR3A; }
+	else if(_PWM1 == 2) { TCCR3A |= COM3B1; _PWM1_REG = &OCR3B; }
+	else if(_PWM1 == 3) { TCCR3A |= COM3C1; _PWM1_REG = &OCR3C; }
+	
+	if(_PWM2 == 5)      { TCCR3A |= COM3A1; _PWM2_REG = &OCR3A; }
+	else if(_PWM2 == 2) { TCCR3A |= COM3B1; _PWM2_REG = &OCR3B; }
+	else if(_PWM2 == 3) { TCCR3A |= COM3C1; _PWM2_REG = &OCR3C; }
+	
+	//TCCR3A = COM3A1 + COM3B1 + COM3C1;
+    TCCR3B = WGM33 + CS30;
+    ICR3 = 400;
+  }
+  else if(_PWM1 == 6 || _PWM1 == 7 || _PWM1 == 8 || 
+          _PWM2 == 6 || _PWM2 == 7 || _PWM2 == 8)
+  {
+    //timer4  
+	TCCR4A = 0;
+	if(_PWM1 == 6)      { TCCR4A |= COM4A1; _PWM1_REG = &OCR4A; }
+	else if(_PWM1 == 7) { TCCR4A |= COM4B1; _PWM1_REG = &OCR4B; }
+	else if(_PWM1 == 8) { TCCR4A |= COM4C1; _PWM1_REG = &OCR4C; }
+	
+	if(_PWM2 == 6)      { TCCR4A |= COM4A1; _PWM2_REG = &OCR4A; }
+	else if(_PWM2 == 7) { TCCR4A |= COM4B1; _PWM2_REG = &OCR4B; }
+	else if(_PWM2 == 8) { TCCR4A |= COM4C1; _PWM2_REG = &OCR4C; }
+	
+	//TCCR4A = COM4A1 + COM4B1 + COM4C1;
+    TCCR4B = WGM43 + CS40;
+    ICR4 = 400;
+  }  
+  else if(_PWM1 == 44 || _PWM1 == 45 || _PWM1 == 46 || 
+          _PWM2 == 44 || _PWM2 == 45 || _PWM2 == 46)
+  {
+	//timer5
+	TCCR5A = 0;
+	if(_PWM1 == 46)      { TCCR5A |= COM5A1; _PWM1_REG = &OCR5A; }
+	else if(_PWM1 == 45) { TCCR5A |= COM5B1; _PWM1_REG = &OCR5B; }
+	else if(_PWM1 == 44) { TCCR5A |= COM5C1; _PWM1_REG = &OCR5C; }
+	
+	if(_PWM2 == 46)      { TCCR5A |= COM5A1; _PWM2_REG = &OCR5A; }
+	else if(_PWM2 == 45) { TCCR5A |= COM5B1; _PWM2_REG = &OCR5B; }
+	else if(_PWM2 == 44) { TCCR5A |= COM5C1; _PWM2_REG = &OCR5C; }
+	
+	//TCCR5A = COM5A1 + COM5B1 + COM5C1;
+    TCCR5B = WGM53 + CS50;
+    ICR5 = 400;
+  }
   #endif
 }
 // Set speed for motor 1, speed is a number betwenn -400 and 400
@@ -71,11 +162,12 @@ void DualVNH5019MotorShield::setM1Speed(int speed)
   }
   if (speed > 400)  // Max PWM dutycycle
     speed = 400;
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
-  OCR1A = speed;
-  #else
-  analogWrite(_PWM1,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif
+  if(_PWM1_REG) {
+	*_PWM1_REG = speed; // if the register is set then use the timers
+  } else {
+	analogWrite(_PWM1,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
+  }
+
   if (speed == 0)
   {
     digitalWrite(_INA1,LOW);   // Make the motor coast no
@@ -105,11 +197,13 @@ void DualVNH5019MotorShield::setM2Speed(int speed)
   }
   if (speed > 400)  // Max 
     speed = 400;
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
-  OCR1B = speed;
-  #else
-  analogWrite(_PWM2,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif 
+
+  if(_PWM2_REG) {
+	*_PWM2_REG = speed; // if the register is set then use the timers
+  } else {
+	analogWrite(_PWM1,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
+  }
+	
   if (speed == 0)
   {
     digitalWrite(_INA2,LOW);   // Make the motor coast no
@@ -146,11 +240,11 @@ void DualVNH5019MotorShield::setM1Brake(int brake)
     brake = 400;
   digitalWrite(_INA1, LOW);
   digitalWrite(_INB1, LOW);
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
-  OCR1A = brake;
-  #else
-  analogWrite(_PWM1,brake * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif
+  if(_PWM1_REG) {
+	*_PWM1_REG = brake; // if the register is set then use the timers
+  } else {
+	analogWrite(_PWM1,brake * 51 / 80); // default to using analogWrite, mapping 400 to 255
+  }
 }
 
 // Brake motor 2, brake is a number between 0 and 400
@@ -165,11 +259,11 @@ void DualVNH5019MotorShield::setM2Brake(int brake)
     brake = 400;
   digitalWrite(_INA2, LOW);
   digitalWrite(_INB2, LOW);
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
-  OCR1B = brake;
-  #else
-  analogWrite(_PWM2,brake * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif
+  if(_PWM1_REG) {
+	*_PWM1_REG = brake; // if the register is set then use the timers
+  } else {
+	analogWrite(_PWM1,brake * 51 / 80); // default to using analogWrite, mapping 400 to 255
+  }
 }
 
 // Brake motor 1 and 2, brake is a number between 0 and 400
